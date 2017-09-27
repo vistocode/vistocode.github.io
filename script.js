39895d6c6c273c3b8389d222a1b2fa12408b7ac2
John Visto <vistocode@gmail.com>
2017-09-27 23:04:11 +0800

    updated scroll reveal for all sections

--- script.js
+++ script.js
@@ -2,19 +2,49 @@ window.sr = ScrollReveal();
   sr.reveal('.card', {
     duration   : 2000,
     scale      : 1,
-    distance   : '300px',
+    distance   : '150px',
     origin     : 'bottom',
     reset      : true,
     easing     : 'ease-out',
-    viewFactor : 0.5,
+    viewFactor : 0.2,
   });
 
    sr.reveal('.card-title, .card-text', {
     duration   : 500,
     scale      : 1,
-    distance   : '100px',
+    distance   : '50px',
     origin     : 'bottom',
     reset      : true,
     easing     : 'ease-out',
     viewFactor : 1,
+  });
+  
+  sr.reveal('.btn-primary', {
+    duration: 500,
+    delay: 500,
+    distance: '50px',
+    origin: 'bottom',
+    reset: true,
+    easing: 'ease-out',
+    viewFactor: 1,
+  });
+  
+  sr.reveal('.contact--form', {
+    duration: 500,
+    delay: 500,
+    distance: '300px',
+    origin: 'left',
+    reset: true,
+    easing: 'ease-out',
+    viewFactor: 0.2,
+  });
+  
+  sr.reveal('.social-sites', {
+    duration: 500,
+    delay: 500,
+    distance: '300px',
+    origin: 'right',
+    reset: true,
+    easing: 'ease-out',
+    viewFactor: 0.2,
   });
